3d263448fc24095f2d57381259afad3a0b91e8f3
JUAN FRANCISCO MARAPODE <47758862@ort>
2025-05-09 08:46:33 -0300

    Terminado

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleButton() {
+    setCount(count + 1);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./assets/fondo.jpg')} style={styles.background} resizeMode="cover">
+      <View style={styles.container}>
+        <Text style={styles.title}>Primera App React Native</Text>
+        <StatusBar style="light" />
+        <Button onPress={handleButton} title="Cambiar Variable" color="green" />
+        <Text style={styles.count}>{count}</Text>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  title: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  count: {
+    fontSize: 25,
+    color: 'white',
+    marginTop: 10,
+  }
 });
